89293ff26157a72b144d605c73d386e8fbd2feb2
ivanliberato2025 <jhowsuellen57@gmail.com>
2025-08-07 19:38:00 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("respirabem").then(cache => {
+      return cache.addAll([
+        "index.html",
+        "style.css",
+        "manifest.json",
+        "icon-192.png",
+        "icon-512.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
